3ba2a4600e5ab05d6bc96848975929bbf786ae1c
mohamedshiha15@gmail.com <mohamedshiha15@gmail.com>
2022-12-17 22:33:02 +0200

    Base network layer and views for the project.

---
+++ ThirdwayvChallenge/Scenes/ProductsList/Models/Image.swift
@@ -0,0 +1,14 @@
+//
+//  Image.swift
+//  ThirdwayvChallenge
+//
+//  Created by Mohamed Shiha on 12/17/22.
+//
+
+import Foundation
+
+struct Image: Codable {
+	let width: Int
+	let height: Int
+	let url: String
+}
---
+++ ThirdwayvChallenge/Scenes/ProductsList/Service/ProductsList.swift
@@ -0,0 +1,16 @@
+//
+//  ProductsList.swift
+//  ThirdwayvChallenge
+//
+//  Created by Mohamed Shiha on 12/17/22.
+//
+
+import Foundation
+
+enum ProductsListService: Servicable {
+	typealias Requestable = GetRequest
+	case endpoint
+	var request: GetRequest {
+		GetRequest(path: "/k0g4Fz")
+	}
+}
