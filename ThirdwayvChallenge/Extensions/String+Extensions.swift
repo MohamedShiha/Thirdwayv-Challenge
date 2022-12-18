d91f8f28baad4648fa42440e9a2b88a4416b013c
mohamedshiha15@gmail.com <mohamedshiha15@gmail.com>
2022-12-18 21:15:01 +0200

    String extension to calculate container size.

---
+++ ThirdwayvChallenge/Extensions/String+Extensions.swift
@@ -0,0 +1,19 @@
+//
+//  String+Extensions.swift
+//  ThirdwayvChallenge
+//
+//  Created by Mohamed Shiha on 12/18/22.
+//
+
+import Foundation
+
+extension String {
+	func suggestedContaimerSize(in size: CGSize) -> CGSize {
+		let constraintRect = CGSize(width: size.width, height: size.height)
+		let boundingBox = self.boundingRect(with: constraintRect,
+											options: .usesLineFragmentOrigin,
+											attributes: nil,
+											context: nil)
+		return boundingBox.size
+	}
+}
